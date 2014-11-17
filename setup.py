62ab277d2f3e81a8cb80622b286aa9e68abab1a0
arve0 <arve.seljebu@gmail.com>
2014-11-17 15:10:30 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(name='LeicaExperiment',
+      version='0.1',
+      description='Class based approach to reading ome.tifs from Leica LAS Matrix Screener',
+      author='Arve Seljebu',
+      license='MIT',
+      url='https://github.com/arve0/leicaexperiement',
+      py_modules=['leicaexperiment'],
+      install_requires=['tifffile', 'numpy'])
