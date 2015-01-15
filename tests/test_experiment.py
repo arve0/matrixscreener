a8ccbf4156ae3f2ae560dcfcc5d810d46b0c09f8
arve0 <arve.seljebu@gmail.com>
2015-01-15 03:15:07 +0100

    test new experiment

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -2,24 +2,25 @@ import pytest
 
 @pytest.fixture(scope='session')
 def experiment():
-    import matrixscreener
+    from matrixscreener.experiment import Experiment
     from os import path
-    
+
     folder = path.join(path.dirname(__file__), 'experiment--test')
-    return matrixscreener.Experiment(folder)
+    return Experiment(folder)
 
 
 def test_stitch(tmpdir, experiment):
     "It should stitch images without error."
-    well = experiment.wells[0]
-    well.stitch(tmpdir.strpath)
+    files = experiment.stitch(tmpdir.strpath)
 
     # both channels stitched
-    assert len(tmpdir.listdir()) == 2
+    assert files == tmpdir.listdir()
 
 
 def test_looping(experiment):
     for well in experiment.wells:
-        for field in well.fields:
-            for image in field.images:
-                assert image.filename
+        assert type(well) == str
+    for field in experiment.fields:
+        assert type(field) == str
+    for image in experiment.images:
+        assert type(image) == str
