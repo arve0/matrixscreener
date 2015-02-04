b218ca3ea0525545051e35f2963985a3d33d0d29
arve0 <arve.seljebu@gmail.com>
2015-02-04 01:02:57 +0100

    python 2.7 support for compression test

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -32,38 +32,45 @@ def test_compression(experiment):
     from PIL import Image
     import numpy as np
     from copy import copy
-    from tempfile import TemporaryDirectory
-    from os import path, replace
+    from tempfile import mkdtemp # python 2.7 support
+    from os import path, rename, remove, removedirs
+
+    def replace(src, dst):
+        # short hand for replace in python 3.3
+        remove(dst)
+        rename(src,dst)
 
     # make a copy of filenames for easier looping
     tifs = copy(experiment.images)
     pngs = ms.experiment.compress(experiment.images)
 
-    with TemporaryDirectory() as tmp:
-        origs = [] # keep data for decompress test
+    tmp = mkdtemp()
+    origs = [] # keep data for decompress test
 
-        # check that compression is lossless
-        for i,(tif,png) in enumerate(zip(tifs, pngs)):
-            origs.append(np.array(Image.open(tif)))
-            compressed = np.array(Image.open(png))
+    # check that compression is lossless
+    for i,(tif,png) in enumerate(zip(tifs, pngs)):
+        origs.append(np.array(Image.open(tif)))
+        compressed = np.array(Image.open(png))
 
-            # move tifs to tmp directory
-            basename = path.basename(tif)
-            replace(tif, path.join(tmp, basename))
+        # move tifs to tmp directory
+        basename = path.basename(tif)
+        rename(tif, path.join(tmp, basename))
 
-            # print some debugging
-            print('check if png image data is identical ' + basename)
-            assert np.all(origs[i] == compressed)
+        # print some debugging
+        print('check if png image data is identical ' + basename)
+        assert np.all(origs[i] == compressed)
 
-        # we are finished with pngs -> delete them on decompress
-        ms.experiment.decompress(experiment.images,
-                                 delete_png=True, delete_json=True)
+    # we are finished with pngs -> delete them on decompress
+    ms.experiment.decompress(experiment.images,
+                             delete_png=True, delete_json=True)
 
-        # check that decompression is lossless
-        for tif,orig in zip(tifs, origs):
-            decompressed = np.array(Image.open(tif))
-            assert np.all(orig == decompressed)
-            # move back
-            basename = path.basename(tif)
-            print('move back image ' + basename)
-            replace(path.join(tmp, basename), tif)
+    # check that decompression is lossless
+    for tif,orig in zip(tifs, origs):
+        decompressed = np.array(Image.open(tif))
+        assert np.all(orig == decompressed)
+        # move back
+        basename = path.basename(tif)
+        print('move back image ' + basename)
+        replace(path.join(tmp, basename), tif)
+
+    removedirs(tmp)
