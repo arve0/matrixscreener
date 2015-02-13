5d5997fb047bbd920445081fadf53bc61daa1baf
arve0 <arve.seljebu@gmail.com>
2015-02-13 01:30:08 +0100

    import utils submodule in init, require fijibin>=0.0.3

--- matrixscreener/__init__.py
+++ matrixscreener/__init__.py
@@ -1,7 +1,7 @@
 """
 Interfacing with Leica LAS AF MatrixScreener.
 """
-__all__ = [ 'cam', 'experiment']
-from matrixscreener import cam, experiment
+__all__ = [ 'cam', 'experiment', 'utils']
+from matrixscreener import cam, experiment, utils
 
 VERSION = '0.5.0'
