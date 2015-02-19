d57d37c599d4c3ffbf6a4cac7991d328314f2a43
arve0 <arve.seljebu@gmail.com>
2015-02-19 12:30:42 +0100

    do not loop non-existent variable

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # show __init__ docstrings
 autoclass_content = 'both'
